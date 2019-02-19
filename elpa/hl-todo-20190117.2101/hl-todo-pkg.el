4e73299a702c096974411334ae9287b96e41b5f6
Michiel Borkent <michielborkent@gmail.com>
2019-02-19 16:43:26 +0100

    Upgrade a bunch of packages

--- elpa/hl-todo-20190117.2101/hl-todo-pkg.el
+++ elpa/hl-todo-20190117.2101/hl-todo-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "hl-todo" "20190117.2101" "highlight TODO and similar keywords" 'nil :commit "099d0f6d583009b0c5de8e51e38972d6e14e0587" :keywords '("convenience") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/hl-todo")
+(define-package "hl-todo" "20190117.2101" "highlight TODO and similar keywords" 'nil :commit "ec44a210a176b879f98c80bf688b0196cb282e7f" :keywords '("convenience") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/hl-todo")
