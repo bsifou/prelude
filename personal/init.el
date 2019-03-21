df1e485b479eb92b6d7d8ff8f2834e582d67f2a4
Michiel Borkent <michielborkent@gmail.com>
2019-03-21 14:00:51 +0100

    Add clj-kondo

--- personal/init.el
+++ personal/init.el
@@ -247,3 +247,18 @@
 
 (custom-set-variables
  '(show-trailing-whitespace t))
+
+(flycheck-define-checker clj-kondo
+  "See `https://github.com/borkdude/clj-kondo'."
+  :command ("clj-kondo" "--lint" "-")
+  :standard-input t
+  :error-patterns
+  ((error line-start "<stdin>:" line ":" column ": " (0+ not-newline) (or "error: " "Exception: ") (message) line-end)
+   (warning line-start "<stdin>:" line ":" column ": " (0+ not-newline) "warning: " (message) line-end))
+  :modes (clojure-mode clojurec-mode clojurescript-mode)
+  :predicate (lambda () (not (string= "edn" (file-name-extension (buffer-file-name)))))
+  ;; use this when you also use the joker linter, recommended!
+  :next-checkers ((error . clojure-joker) (warning . clojure-joker) (warning . clojurescript-joker))
+  )
+
+(add-to-list 'flycheck-checkers 'clj-kondo)
