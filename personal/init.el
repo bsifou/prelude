d492c29f8a29eea3d9d8ca0e4fb9ac30f57b0a02
Michiel Borkent <michielborkent@gmail.com>
2019-03-29 15:35:58 +0100

    Update kondo config

--- personal/init.el
+++ personal/init.el
@@ -248,17 +248,29 @@
 (custom-set-variables
  '(show-trailing-whitespace t))
 
-(flycheck-define-checker clj-kondo
-  "See `https://github.com/borkdude/clj-kondo'."
-  :command ("clj-kondo" "--lint" "-")
+(flycheck-define-checker clj-kondo-clj
+  "See https://github.com/borkdude/clj-kondo"
+  :command ("clj-kondo" "--cache" "--lang" "clj" "--lint" "-")
   :standard-input t
   :error-patterns
   ((error line-start "<stdin>:" line ":" column ": " (0+ not-newline) (or "error: " "Exception: ") (message) line-end)
    (warning line-start "<stdin>:" line ":" column ": " (0+ not-newline) "warning: " (message) line-end))
-  :modes (clojure-mode clojurec-mode clojurescript-mode)
+  :modes (clojure-mode clojurec-mode)
   :predicate (lambda () (not (string= "edn" (file-name-extension (buffer-file-name)))))
   ;; use this when you also use the joker linter, recommended!
-  :next-checkers ((error . clojure-joker) (warning . clojure-joker) (warning . clojurescript-joker))
-  )
+  :next-checkers ((error . clojure-joker) (warning . clojure-joker)))
 
-(add-to-list 'flycheck-checkers 'clj-kondo)
+(flycheck-define-checker clj-kondo-cljs
+  "See https://github.com/borkdude/clj-kondo"
+  :command ("clj-kondo" "--cache" "--lang" "cljs" "--lint" "-")
+  :standard-input t
+  :error-patterns
+  ((error line-start "<stdin>:" line ":" column ": " (0+ not-newline) (or "error: " "Exception: ") (message) line-end)
+   (warning line-start "<stdin>:" line ":" column ": " (0+ not-newline) "warning: " (message) line-end))
+  :modes (clojurescript-mode)
+  :predicate (lambda () (not (string= "edn" (file-name-extension (buffer-file-name)))))
+  ;; use this when you also use the joker linter, recommended!
+  :next-checkers ((error . clojurescript-joker) (warning . clojurescript-joker)))
+
+(add-to-list 'flycheck-checkers 'clj-kondo-clj)
+(add-to-list 'flycheck-checkers 'clj-kondo-cljs)
