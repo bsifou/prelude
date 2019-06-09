9563fc81dc4f2d14c07d7217649216bfecf7188c
Michiel Borkent <michielborkent@gmail.com>
2019-06-09 11:02:43 +0200

    elpa

---
+++ elpa/flycheck-clj-kondo-20190410.1515/flycheck-clj-kondo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-clj-kondo" "20190410.1515" "Add clj-kondo linter to flycheck" '((flycheck "0.18")) :commit "b0d5af10e045d4eeb824058c8dc87f94b376bf1c" :authors '(("Michiel Borkent" . "michielborkent@gmail.com")) :maintainer '("Michiel Borkent" . "michielborkent@gmail.com") :url "https://github.com/borkdude/flycheck-clj-kondo")
