3fd2ad00a7955d529cc207f56cc956a20d8575a0
Michiel Borkent <michielborkent@gmail.com>
2019-07-27 18:02:15 +0200

    elpa

---
+++ elpa/clojure-mode-20190716.623/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20190716.623" "Major mode for Clojure code" '((emacs "25.1")) :commit "5a3c6ea8a91cab0536720e862d571b02462061e3" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
